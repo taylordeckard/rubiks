0b521f6f6e0fe981899ddcb4e901bb185ab8ccb8
taylordeckard <taylor999@gmail.com>
2021-11-19 15:51:47 -0500

    Remove unnecessary comment

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,6 @@ import './App.css';
 
 function App() {
   const [appRef, setAppRef] = useState<HTMLDivElement | null>(null);
-  // const mousePosition = useRef({ x: 0, y: 0 });
-  // const click = useRef({
-  //   cubeX: 0,
-  //   cubeY: 0,
-  //   isDown: false,
-  //   x: 0,
-  //   y: 0,
-  // });
   const main = useRef(new Main());
   useAnimation(main);
   useRenderer(appRef, main);
