536d08f64a98a7f5a8c42d25c703dcae9b09fe14
taylordeckard <taylor999@gmail.com>
2021-11-20 22:04:21 -0500

    Add 27 cubes

---
+++ src/classes/Cubes.tsx
@@ -0,0 +1,53 @@
+import {
+  BoxGeometry,
+  Group,
+  TextureLoader,
+  Mesh,
+  MeshStandardMaterial,
+  Scene,
+} from 'three';
+
+export class Cubes {
+  private faceTexture: {
+    blue: MeshStandardMaterial;
+    green: MeshStandardMaterial;
+    orange: MeshStandardMaterial;
+    red: MeshStandardMaterial;
+    white: MeshStandardMaterial;
+    yellow: MeshStandardMaterial;
+  };
+  private cubes: Mesh[] = [];
+  public all: Group;
+  constructor (scene: Scene) {
+    const loader = new TextureLoader();
+    loader.setPath( 'textures/' );
+    this.faceTexture = {
+      blue: new MeshStandardMaterial({ map: loader.load('face_blue.png') }),
+      red: new MeshStandardMaterial({ map: loader.load('face_red.png') }),
+      orange: new MeshStandardMaterial({ map: loader.load('face_orange.png') }),
+      white: new MeshStandardMaterial({ map: loader.load('face_white.png') }),
+      green: new MeshStandardMaterial({ map: loader.load('face_green.png') }),
+      yellow: new MeshStandardMaterial({ map: loader.load('face_yellow.png') }),
+    };
+    const geometry = new BoxGeometry();
+    this.all = new Group();
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        for (let k = -1; k < 2; k++) {
+          const cube = new Mesh(geometry, [
+            this.faceTexture.blue,
+            this.faceTexture.green,
+            this.faceTexture.yellow,
+            this.faceTexture.white,
+            this.faceTexture.orange,
+            this.faceTexture.red,
+          ]);
+          cube.position.set(i, j, k);
+          this.cubes.push(cube);
+          this.all.add(cube);
+        }
+      }
+    }
+    scene.add(this.all);
+  }
+}
--- src/classes/Main.tsx
+++ src/classes/Main.tsx
@@ -1,49 +1,33 @@
 import {
-  BoxGeometry,
   Color,
-  TextureLoader,
+  Group,
   HemisphereLight,
-  Mesh,
-  MeshStandardMaterial,
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
 } from 'three';
 import { MouseHandler } from './MouseHandler';
+import { Cubes } from './Cubes';
 
 export class Main {
   public camera: PerspectiveCamera;
-  public cube: Mesh;
+  public cube: Group;
+  public cubes: Cubes;
   public renderer: WebGLRenderer;
   public scene: Scene;
   public light: HemisphereLight;
   public mouseHandler: MouseHandler;
   constructor () {
-    const loader = new TextureLoader();
-    loader.setPath( 'textures/' );
-    const blueTexture = loader.load('face_blue.png');
-    const redTexture = loader.load('face_red.png');
-    const orangeTexture = loader.load('face_orange.png');
-    const whiteTexture = loader.load('face_white.png');
-    const greenTexture = loader.load('face_green.png');
-    const yellowTexture = loader.load('face_yellow.png');
     this.scene = new Scene();
     this.scene.background = new Color(0x242424);
-    this.camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    this.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.renderer = new WebGLRenderer();
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
-    const geometry = new BoxGeometry();
-    const blueFace = new MeshStandardMaterial( {  map: blueTexture } );
-    const redFace = new MeshStandardMaterial( {  map: redTexture } );
-    const orangeFace = new MeshStandardMaterial( {  map: orangeTexture } );
-    const whiteFace = new MeshStandardMaterial( {  map: whiteTexture } );
-    const greenFace = new MeshStandardMaterial( {  map: greenTexture } );
-    const yellowFace = new MeshStandardMaterial( {  map: yellowTexture } );
-    this.cube = new Mesh(geometry, [blueFace, redFace, orangeFace, whiteFace, greenFace, yellowFace]);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.cubes = new Cubes(this.scene);
+    this.cube = this.cubes.all;
     this.light = new HemisphereLight(0xFFFFFF, 0xDDDDDD, 1);
-    this.scene.add(this.cube);
     this.scene.add(this.light);
-    this.camera.position.z = 2;
+    this.camera.position.z = 5;
     this.mouseHandler = new MouseHandler(this);
   }
 }
